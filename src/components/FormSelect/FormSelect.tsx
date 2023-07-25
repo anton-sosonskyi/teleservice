887a767b85538dcd08e28b3629b8f38ffdef02cd
Anton Sosonskyi <114813220+anton-sosonskyi@users.noreply.github.com>
2023-07-25 10:02:13 +0300

    TRA-22: Replace all any in the project to strict TypeScript types (#9)

--- src/components/FormSelect/FormSelect.tsx
+++ src/components/FormSelect/FormSelect.tsx
@@ -1,55 +1,59 @@
 import { FormControl, FormHelperText } from "@mui/material";
 import { MenuItem, Select } from "@mui/material";
-import { Controller } from "react-hook-form";
+import { Control, Controller, FieldValues, Path } from "react-hook-form";
 import { ArrowDownIcon } from "./icons/ArrowDownIcon";
 
-type Props = {
-  name: string;
-  control: any;
+type Props<T extends FieldValues> = {
+  name: Path<T>;
+  control: Control<T>;
   variant?: "standard" | "outlined" | "filled" | undefined;
   placeholder?: string;
   optionList: { value: string; label: string }[];
 };
 
-export const FormSelect: React.FC<Props> = ({
+export const FormSelect = <T extends FieldValues>({
   name,
   control,
   variant,
   placeholder,
   optionList,
-}) => {
+}: Props<T>) => {
   return (
-    <Controller
-      name={name}
-      control={control}
-      render={({ field, fieldState, formState }) => (
-        <FormControl>
-          <Select
-            {...field}
-            style={{ height: 42 }}
-            SelectDisplayProps={{ style: { paddingTop: 8, paddingBottom: 8 } }}
-            variant={variant}
-            value={field.value || ""}
-            IconComponent={ArrowDownIcon}
-            displayEmpty
-            error={fieldState.invalid}
-          >
-            <MenuItem disabled value="">
-              <span className="text-[#757474]">{placeholder}</span>
-            </MenuItem>
-
-            {optionList.map(({ value, label }) => (
-              <MenuItem key={value} value={value}>
-                {label}
+    <FormControl>
+      <Controller
+        name={name}
+        control={control}
+        render={({ field, fieldState, formState }) => (
+          <>
+            <Select
+              onChange={(e) => field.onChange(e.target.value as T[keyof T])}
+              style={{ height: 42 }}
+              SelectDisplayProps={{
+                style: { paddingTop: 8, paddingBottom: 8 },
+              }}
+              variant={variant}
+              value={field.value || ""}
+              IconComponent={ArrowDownIcon}
+              displayEmpty
+              error={fieldState.invalid}
+            >
+              <MenuItem disabled value="">
+                <span className="text-[#757474]">{placeholder}</span>
               </MenuItem>
-            ))}
-          </Select>
 
-          <FormHelperText>
-            {formState.errors[name]?.message as string}
-          </FormHelperText>
-        </FormControl>
-      )}
-    />
+              {optionList.map(({ value, label }) => (
+                <MenuItem key={value} value={value}>
+                  {label}
+                </MenuItem>
+              ))}
+            </Select>
+
+            <FormHelperText>
+              {formState.errors[name]?.message as string}
+            </FormHelperText>
+          </>
+        )}
+      />
+    </FormControl>
   );
 };
